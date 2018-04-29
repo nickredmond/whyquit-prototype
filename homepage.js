13012afcc12d033c86b8d0c2d273f1cc8f51f782
Nick Redmond <nicholas.john.redmond@gmail.com>
2018-04-29 09:00:14 -0600

    added call to get secondary stories

--- homepage.js
+++ homepage.js
@@ -6,73 +6,85 @@
 // 	})
 // };
 
-var getTopStories = function(onTopStoriesReturned) {
-	var url = ENVIRONMENT["HOST"] + "/whyquit/top-stories";
+var getStories = function(onStoriesReturned, path) {
+	var url = ENVIRONMENT["HOST"] + "/" + path;
 	$.get(url, function(topStories) {
-		onTopStoriesReturned(topStories);
+		onStoriesReturned(topStories);
 	});
 }
-
-var hexToBase64 = function(str) {
-    return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+var getTopStories = function(onTopStoriesReturned) {
+	getStories(onTopStoriesReturned, "whyquit/top-stories");
+};
+var getSecondaryStories = function(onSecondaryStoriesReturned) {
+	getStories(onSecondaryStoriesReturned, "whyquit/secondary-stories");
 };
 
-document.addEventListener("DOMContentLoaded", function(event) {
+var getMaxCarouselHeight = function() {
 	var windowHeight = window.innerHeight;
 	var logoHeight = $("#mainSiteLogo").height();
 	var BUFFER_HEIGHT = 75;
-	var maxCarouselHeight = windowHeight - logoHeight - BUFFER_HEIGHT;
+	return windowHeight - logoHeight - BUFFER_HEIGHT;
+};
 
-	getTopStories(function(topStories) {
-		topStories.forEach(function(topStory, index) {	
-			console.log("the story: " + JSON.stringify(topStory));
-			var slide = document.createElement("div");
-			if (index == 0) {
-				slide.classList.add("carousel-item", "active");
-			}
-			else {
-				slide.classList.add("carousel-item");
-			}
-
-			var imageAnchor = document.createElement("a");
-			imageAnchor.setAttribute("href", topStory["whyquit_link"]);
-			imageAnchor.setAttribute("target", "_blank");
-
-			var slideImg = new Image(); 
-
-			slideImg.onload = function(){
-				slideImg.style.maxHeight = maxCarouselHeight + "px";
-				var scalingFactor = maxCarouselHeight / slideImg.height;
-				slideImg.style.maxWidth = (slideImg.width * scalingFactor) + "px";
-				imageAnchor.appendChild(slideImg);
-			};
-
-			slideImg.src = ENVIRONMENT["HOST"] + "/whyquit/images/" + topStory["imageFilename"];
-			slideImg.classList.add("d-block", "w-75", "carousel-image");
-
-			var caption = document.createElement("div");
-			caption.classList.add("w-75", "top-story-caption-block");
-			var captionText = document.createElement("h5");
-			captionText.innerHTML = topStory["title"];
-			caption.appendChild(captionText);
-
-			slide.appendChild(imageAnchor);
-			slide.appendChild(caption);
-
-			var slidesContainer = document.getElementById("slidesContainer");
-			slidesContainer.appendChild(slide);
-		});
-		
-		var indicators = document.getElementById("tobaccoDeathsCarouselIndicators");
-		for (var i = 0; i < topStories.length; i++) {
-			var indicator = document.createElement("li");
-			indicator.setAttribute("data-target", "#tobaccoDeathsCarouselIndicators");
-			indicator.setAttribute("data-slide-to", i);
-
-			if (i == 0) {
-				indicator.classList.add(["active"]);
-			}
-			indicators.appendChild(indicator);
+var addTopStoriesToPage = function(topStories) {
+	topStories.forEach(function(topStory, index) {	
+		console.log("the story: " + JSON.stringify(topStory));
+		var slide = document.createElement("div");
+		if (index == 0) {
+			slide.classList.add("carousel-item", "active");
 		}
+		else {
+			slide.classList.add("carousel-item");
+		}
+
+		var imageAnchor = document.createElement("a");
+		imageAnchor.setAttribute("href", topStory["whyquit_link"]);
+		imageAnchor.setAttribute("target", "_blank");
+
+		var slideImg = new Image(); 
+
+		var maxCarouselHeight = getMaxCarouselHeight();
+		slideImg.onload = function(){
+			slideImg.style.maxHeight = maxCarouselHeight + "px";
+			var scalingFactor = maxCarouselHeight / slideImg.height;
+			slideImg.style.maxWidth = (slideImg.width * scalingFactor) + "px";
+			imageAnchor.appendChild(slideImg);
+		};
+
+		slideImg.src = ENVIRONMENT["HOST"] + "/whyquit/images/" + topStory["imageFilename"];
+		slideImg.classList.add("d-block", "w-75", "carousel-image");
+
+		var caption = document.createElement("div");
+		caption.classList.add("w-75", "top-story-caption-block");
+		var captionText = document.createElement("h5");
+		captionText.innerHTML = topStory["title"];
+		caption.appendChild(captionText);
+
+		slide.appendChild(imageAnchor);
+		slide.appendChild(caption);
+
+		var slidesContainer = document.getElementById("slidesContainer");
+		slidesContainer.appendChild(slide);
 	});
+	
+	var indicators = document.getElementById("tobaccoDeathsCarouselIndicators");
+	for (var i = 0; i < topStories.length; i++) {
+		var indicator = document.createElement("li");
+		indicator.setAttribute("data-target", "#tobaccoDeathsCarouselIndicators");
+		indicator.setAttribute("data-slide-to", i);
+
+		if (i == 0) {
+			indicator.classList.add(["active"]);
+		}
+		indicators.appendChild(indicator);
+	}
+};
+
+var addSecondaryStoriesToPage = function(secondaryStories) {
+	console.log("secondary " + JSON.stringify(secondaryStories));
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	getTopStories(addTopStoriesToPage);
+	getSecondaryStories(addSecondaryStoriesToPage);
 });
